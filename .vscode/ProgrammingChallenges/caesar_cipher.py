de7687688c7e1cfc7067ee984e6634d7dc2bada2
gwal-909 <gwalker.btc@gmail.com>
2018-09-27 22:14:46 -0700

    add caesar cipher

---
+++ .vscode/ProgrammingChallenges/caesar_cipher.py
@@ -0,0 +1,24 @@
+def ceasar_cipher(in_text, shift):
+    result_text = ''
+
+    for letter in range(len(in_text)):
+        char = in_text[letter]
+
+        if char.isupper():
+            result_text += chr((ord(char) + shift - 65) % 36 + 65)
+            # result_text += chr((ord(char) + shift) % 36)
+        else:
+            result_text += chr((ord(char) + shift - 97) % 26 + 97)
+            # result_text += chr((ord(char) + shift) % 26)
+    return result_text
+
+def main():
+    text = 'WeAreDOOMED'
+    shift = 4
+    ciphered_message = ceasar_cipher(text, shift)
+    print('Input: ', text)
+    print('Shift count: ', shift)
+    print('Ciphered text: ', ciphered_message)  
+
+if __name__ == '__main__':
+    main()
