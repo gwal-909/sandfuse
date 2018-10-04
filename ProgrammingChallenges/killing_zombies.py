07bd70c6db0edd8faac6332369a409321e8fa5a7
gwal-909 <gwalker.btc@gmail.com>
2018-10-04 16:50:25 -0700

    add script for new challenge

---
+++ ProgrammingChallenges/killing_zombies.py
@@ -0,0 +1,7 @@
+"""code challenge from code chef.
+you can find the challenge discription here:
+https://www.codechef.com/problems/CLKLZM 
+"""
+
+n = 0 # number of zombies
+m = 0 # number of cranes
