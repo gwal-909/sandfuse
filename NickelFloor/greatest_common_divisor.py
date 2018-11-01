c1d436caf58c11046249bcd66537b0e5ec3d209c
gwal-909 <gwalker.btc@gmail.com>
2018-11-01 11:49:25 -0700

    update edits

--- NickelFloor/greatest_common_divisor.py
+++ NickelFloor/greatest_common_divisor.py
@@ -1,12 +1,12 @@
 def gcd(a, b):
     while b != 0:
-        remainder = a % b
-        a = b
-        b = remainder
-    return a
+        remainder = a % b # get the remainder of dividing both numbers.
+        a = b             # make a (first number) equal to b (second number). 
+        b = remainder     # make b equal to the remainder
+    return a              # return a (was the first number)
 
 def main():
-    ans = gcd(105, 25)
+    ans = gcd(5425, 145)
     print(ans)
 
 if __name__ == '__main__':
--- NickelFloor/lcs_prac.py
+++ NickelFloor/lcs_prac.py
@@ -1,43 +1,36 @@
 def lcs(a, b):
-    
-    # create a two-dimensional array and populate with zeros. 
-    # add 1 for both row and column, the two-dimensional array should have
-    # the first row and first column will be padded with zeros 
     matrix = [[0 for col in range(len(b)+1)] for row in range(len(a)+1)]
-    
-    # nested for loop to iterate over columns for each row.
+
     for row, a_val in enumerate(a):
         for col, b_val in enumerate(b):
             if a_val == b_val:
-                matrix[row+1][col+1] = matrix[row][col] + 1
+                matrix[row+1][col+1] = matrix[row][col] +1
             else:
-                matrix[row+1][col+1] = max(matrix[row][col+1], matrix[row+1][col])
-
+                matrix[row+1][col+1] = max(matrix[row+1][col], matrix[row][col+1])
+             
     x = len(a)
     y = len(b)
 
     result = ''
 
-    # while loop to traverse the matrix and build the lcs result.
     while x != 0 and y != 0:
         if matrix[x][y] == matrix[x-1][y]:
             x -= 1
-        elif matrix[x][y] == matrix[x][y-1]:
+        elif matrix[x][y] == matrix[x][-1]:
             y -= 1
         else:
             assert a[x-1] == b[y-1]
-            result = a[x-1] + result # place the matched indexed latter at front of string
-            # traverse matrix diagonally up left
+            result = a[x-1] + result
+            y -= 1
             x -= 1
-            y -= 1 
 
     return result
 
 def main():
-    str1 = 'aqwdnfh awioawienfha;lohxfpaohnf'
-    str2 = 'sacdf ;lif;ainsaswiuenfglzskunhcc'
-    answer = lcs(str1, str2)
-    print(answer)
+    str1 = 'xyzabc'
+    str2 = 'abc'
+    ans = lcs(str1, str2)
+    print(ans)
 
 if __name__ == '__main__':
     main()
